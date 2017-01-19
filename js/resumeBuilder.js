a095817f92d0b8b4e0e849607007346046f03148
Aishwarya A N <aishwarya.an95@gmail.com>
2017-01-19 18:26:39 +0530

    Built the header of the page containing information about myself.
    * Created a JavaScript object in the resumeBuilder file containing information about my role, skills and contacts.
    * Added the images needed by the JavaScript object to the images folder.
    * Changed some styles in the styles.css file regarding the skills list.
    * Changed the skills list in helper.js file to include images.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,44 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name" : "Aishwarya A N",
+  "role" : "Front-end Web Developer",
+  "contacts" : {
+    "mobile" : "9845578144",
+    "email" : "aishwarya.an95@gmail.com",
+    "github" : "aishwarya-an",
+    "location" : "Rajajinagar, Bengaluru"
+  },
+  "welcomeMessage" : "",
+  "skills" : ["images/logo-html5.svg", "images/logo-css3.svg", "images/logo-javascript.svg", "images/logo-jquery.svg", "images/logo-git.svg"],
+  "biopic" : "images/biopic.png"
+}
+
+bio.display = function(){
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+  $("#topContacts").append(formattedMobile);
+  $("#topContacts").append(formattedEmail);
+  $("#topContacts").append(formattedGithub);
+  $("#topContacts").append(formattedLocation);
+  $("#header").append(formattedBiopic);
+  if(bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+    for(var i = 0; i < bio.skills.length; i++){
+      var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+      $("#skills").append(formattedSkills);
+    }
+  }
+  $("#footerContacts").append(formattedMobile);
+  $("#footerContacts").append(formattedEmail);
+  $("#footerContacts").append(formattedGithub);
+  $("#footerContacts").append(formattedLocation);
+}
+
+bio.display();
+
