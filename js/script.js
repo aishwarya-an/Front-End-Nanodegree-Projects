98498163a5137fb456934b7b001d09ed97f64abf
Aishwarya A N <aishwarya.an95@gmail.com>
2017-02-13 19:12:23 +0530

    Added the knockout functionality to implement the filter functionality.
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 var map;
-var markers = [];
 
 // Storing the locations
 var locations = [
-  {title: "Fortis Hospital", location: {lat: 12.894531, lng: 77.598903}},
-  {title: "Manipal Hospital", location: {lat: 12.958900, lng: 77.649198}},
+  {title: "Fortis HealthCare", location: {lat: 12.894531, lng: 77.598903}},
   {title: "NIMHANS", location:{lat: 12.940682, lng: 77.598264}},
-  {title: "HOSMAT Hospital", location: {lat: 12.969034, lng: 77.61346}},
   {title: "Columbia Asia", location: {lat: 13.050987, lng: 77.593789}},
-  {title: "Sagar Hospitals", location: {lat: 12.907708, lng: 77.565115}},
-  {title: "Vikram Hospital", location: {lat: 12.988192, lng: 77.592169}},
   {title: "Apollo Hospital", location: {lat: 12.896128, lng: 77.598535}},
-  {title: "Mallya Hospital", location: {lat: 12.968064, lng: 77.595068}}
+  {title: "BGS Global Hospital", location: {lat: 12.902874, lng: 77.497798}}
 ];
 
 
@@ -19,54 +14,121 @@ var locations = [
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 12.968064, lng: 77.595068},
+    center: {lat: 12.940682, lng: 77.598264},
     zoom: 13
   });
-
-  var largeInfowindow = new google.maps.InfoWindow();
-  var bounds = new google.maps.LatLngBounds();
-
-  // The following group uses the location array to create an array of markers 
-  // on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    // Push the marker to the array of markers.
-    markers.push(marker);
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-    bounds.extend(markers[i].position);
-  }
-
-  // Extend the boundaries of the map for each marker
-  map.fitBounds(bounds);
+  ko.applyBindings(new ViewModel());
 }
 
-// This function populates the infowindow when the marker is clicked.
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker = null;
-    });
+
+// @description The viewmodel
+function ViewModel (){
+    var infoWindow = new google.maps.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+    
+    // @description Hospital object contains the marker and functionality
+    // to populate infowindow.
+    function Hospital(obj) {
+      var self = this;
+      self.title = obj.title;
+      self.latitude = obj.location.lat;
+      self.longitude = obj.location.lng;
+      self.marker = new google.maps.Marker({
+        title: self.title,
+        position: {lat: self.latitude, lng: self.longitude},
+        map: map 
+      });
+
+      // @description When clicked on the marker the infoWindow pops.        
+      self.marker.addListener('click', function() {
+          self.populateInfoWindow(self.marker, infoWindow);
+          
+       });
+
+      // @description To pop the infowindow
+      self.populateInfoWindow = function(marker, infoWindow) {
+        // Checking whether the infowwindow is open
+        if (infoWindow.marker != marker) {
+          infoWindow.marker = marker;
+          // Add animation when clicked 
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+            
+          // Info in the infowindow
+          var content = '<h2>' + self.title + '</h2><h3>Wiki Links</h3><ul>';
+          infoWindow.setContent(content);
+          // The wiki link in the infowindow  
+          var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + marker.title + "&format=json&callback=wikiCallback";
+          $.ajax(url, {
+            dataType: 'jsonp',
+            jsonp: "callback",
+            success: function(data){
+              var list = data[1];
+              var article = list[0];
+              var newContent = infoWindow.getContent() + "<li><a href='http://en.wikipedia.org/wiki/" + article + "'>" + marker.title + "</a></li></ul>";
+              infoWindow.setContent(newContent);
+            }, error: function(){
+                var newContent = infoWindow.getcontent() + 
+                    "<p>Failed to load the wiki links</p>";
+                infoWindow.setContent(newContent);
+              }
+          });
+                                      
+          // Pop up the infowindow
+          infoWindow.open(map, marker);
+          // When closed, the animation should stop.
+          infoWindow.addListener('closeclick', function() {
+            infoWindow.marker = null;
+            marker.setAnimation(null);
+          });
+        }
+      };
+      // End of Hsopital object.
+    }
+    
+  var self = this;
+  self.list = ko.observableArray([]);
+  self.filter = ko.observable('');
+    
+  // Renders all the locations.
+  function render(location) {
+    // Creates Hospital objects and stores it in self.list
+    var current;
+    var currentList = [];
+    for(var i = 0; i < location.length; i++){
+        current = new Hospital(location[i]);
+        currentList.push(current);
+        bounds.extend(current.marker.position);
+    }
+    self.list(currentList);
+    map.fitBounds(bounds);
   }
+
+  self.filterList = ko.computed(function(){
+    // An array storing all the filitered list
+    var filtered = [];
+    for(var i = 0; i < self.list().length; i++){
+      var title = self.list()[i].title.toLowerCase();
+      // Checking if the filter matches the objects in the list
+      if(title.indexOf(self.filter()) != -1){
+        filtered.push(self.list()[i]);
+        self.list()[i].marker.setVisible(true);
+      }
+      else
+        self.list()[i].marker.setVisible(false);
+    }
+    return filtered;
+  });
+  
+  // Calling the render function to render all objects in the list
+  render(locations);
+  
+  // The infowindow must pop up when an object in the list is clicked
+  self.loadWindow = function (hospital){
+      hospital.populateInfoWindow(hospital.marker, infoWindow);
+  };
 }
 
+// @description Shows error when Google Map API is not loaded.
 function showError(){
   alert("Failed loading the Google Map API!!!");
 }
