d3a67913ce4d76b5d423ac166ec9bad98110172e
Aishwarya A N <aishwarya.an95@gmail.com>
2017-02-13 12:55:39 +0530

    Added a simple map functionality to the page.
    * script.js contains the map functionality.
    * Added the styles.css to style the elements in the page.
    * Added jQuery library.
    
    Signed-off-by: Aishwarya A N <aishwarya.an95@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var map;
+var markers = [];
+
+// @description Creates a map
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13
+  });
+}
+
+function showError(){
+  alert("Failed loading the Google Map API!!!");
+}
